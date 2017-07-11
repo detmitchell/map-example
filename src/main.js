08ad0069a5d1d474e14513945c0ee46e07ee8fd3
Jeffrey Mitchell <detmitchell@gmail.com>
2017-07-11 13:44:41 -0500

    added map

--- src/main.js
+++ src/main.js
@@ -1,1 +1,8 @@
 var L = require('leaflet');
+
+var mymap = L.map('mapid').setView([41.87, -87.62], 13);
+
+L.tileLayer( 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+  subdomains: ['a', 'b', 'c']
+}).addTo(mymap)
